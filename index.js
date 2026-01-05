0da752d95fcd4b43334b3975f3484041d07d33b4
lucas <marcopolo1254rn@gmail.com>
2026-01-05 20:41:09 -0300

    Adding user input to the display and making CLEAR button functional

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ let secondNum = 0;
 let operand = '';
 
 const buttons = document.querySelector('.buttons');
+const display = document.querySelector('.display')
 const buttonTextContent = ['7','8','9','+','4','5','6','-','1','2','3','*','CLEAR','0','=','/'];
 
 for(let i=0; i<=15; i++){
     const button = document.createElement('button');
+    button.addEventListener("click", fillingDisplay)
     button.textContent = buttonTextContent[i];
+    // Can add a ifs to clange style of operands, clear, equal and other... (remember to add later)
     button.classList.add('button');
     buttons.appendChild(button);
 };
 
+function fillingDisplay(e){
+    if (e.target.textContent === 'CLEAR') {
+        display.textContent = '';
+        return
+    } 
+    display.textContent += e.target.textContent
+}
 
+function dealingWithUserInput () {
+
+};
 
 const operations = {
     sum(a,b) {
