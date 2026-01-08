9ff221a81208cf34bfad8c0925310e92513a419c
lucas <marcopolo1254rn@gmail.com>
2026-01-08 11:40:30 -0300

    operations results completed and are shown in display

--- index.js
+++ index.js
@@ -5,26 +5,11 @@ let operand = '';
 const buttons = document.querySelector('.buttons');
 const display = document.querySelector('.display')
 const buttonTextContent = ['7','8','9','+','4','5','6','-','1','2','3','*','CLEAR','0','=','/'];
-
-for(let i=0; i<=15; i++){
-    const button = document.createElement('button');
-    button.addEventListener("click", fillingDisplay)
-    button.textContent = buttonTextContent[i];
-    // Can add a ifs to clange style of operands, clear, equal and other... (remember to add later)
-    button.classList.add('button');
-    buttons.appendChild(button);
-};
-
-function fillingDisplay(e){
-    if (e.target.textContent === 'CLEAR') {
-        display.textContent = '';
-        return
-    } 
-    display.textContent += e.target.textContent
-}
-
-function dealingWithUserInput () {
-
+const operatorMap = {
+  "+": "sum",
+  "-": "subtract",
+  "*": "multiply",
+  "/": "divide",
 };
 
 const operations = {
@@ -40,6 +25,38 @@ const operations = {
     divide(a,b){
         return b ===0 ? "Invalid" : (a/b).toFixed(3);
     }
+}
+
+for(let i=0; i<=15; i++){
+    const button = document.createElement('button');
+    button.addEventListener("click", fillingDisplay)
+    button.textContent = buttonTextContent[i];
+    // Can add a ifs to clange style of operands, clear, equal and other... (remember to add later)
+    button.classList.add('button');
+    buttons.appendChild(button);
+};
+
+function fillingDisplay(e){
+    if (e.target.textContent === 'CLEAR') {
+        display.textContent = '';
+        return
+    }
+    if (e.target.textContent === '='){
+        const userInput = display.textContent;
+        const userInputArray = userInput.match(/\d+|[+\-*/]/g)
+        .map(v => isNaN(v) ? v : Number(v));
+        firstNum = userInputArray [0];
+        operand = userInputArray [1];
+        secondNum = userInputArray [2];
+        display.textContent = executeOperation (firstNum,operand,secondNum);         
+        return 
+    } 
+    display.textContent += e.target.textContent
+}
+
+function executeOperation (a,operator,b) {
+    const method = operatorMap[operator];
+    return operations[method](a,b);
 }
 
 console.log(operations.divide(0.3,1.92))
